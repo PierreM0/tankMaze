4a9e52cceb4634428d2795dcef435f9db5431a79
Devin PierreEmmanuel <pierre-emmanuel.devin2@etu.univ-lorraine.fr>
2024-03-03 15:57:58 +0100

    changed config to one sigle file

--- tankMaze/core/src/com/mygdx/game/model/World.java
+++ tankMaze/core/src/com/mygdx/game/model/World.java
@@ -1,24 +1,42 @@
 package com.mygdx.game.model;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Json;
 import com.mygdx.game.controller.Direction;
 import com.mygdx.game.model.gameelement.GameElement;
-import com.mygdx.game.model.gameelement.elementdynamique.TankJoueur;
-import com.mygdx.game.model.gameelement.elementdynamique.TankNpc;
+import com.mygdx.game.model.gameelement.elementdynamique.*;
 import com.mygdx.game.model.gameelement.elementmouvementlineaire.Obus;
+import com.mygdx.game.model.gameelement.elementstatique.ElementVide;
 import com.mygdx.game.model.gameelement.elementstatique.elementdur.ElementDur;
+import com.mygdx.game.model.gameelement.elementstatique.elementdur.MurBrique;
+import com.mygdx.game.model.gameelement.elementstatique.elementdur.MurFer;
 import jdk.jfr.internal.test.WhiteBox;
 
 import java.util.ArrayList;
+import java.util.Random;
 
+class Config {
+    public float[][] npcStart;
+    public int obusMax;
+    public float obusSpeed;
+    public int plane;
+    public int playerWidth;
+    public int playerHeight;
+    public int playerSpeed;
+    public int[] playerPos;
+}
 
 /**
  * World is the main model class, it stores all the game data.
  * It auto-load the config from "assets/config/" at instantiation.
  */
 public class World {
+
+    float intervaleAvion = 0;
+    float elapsedPlaneTime = 0;
 
     float bulletSpeed = 0;
     int bulletMax = 0;
@@ -27,6 +45,49 @@ public class World {
 
     private ArrayList<TankNpc> npcs = new ArrayList<TankNpc>();
     private ArrayList<Obus> obus = new ArrayList<Obus>();
+
+    private ArrayList<Avion> avions = new ArrayList<Avion>();
+
+    public ArrayList<Avion> getAvions() {
+        return avions;
+    }
+
+    void remplacerGrid(int i, int j, GameElement remplacant) {
+        GameElement ge = grid[i][j];
+        remplacant.setY(ge.getY());
+        remplacant.setX(ge.getX());
+        remplacant.setWidth(ge.getWidth());
+        remplacant.setHeight(ge.getHeight());
+        grid[i][j] = remplacant;
+    }
+
+    public void updateObus(float deltaTime) {
+        for (int i = 0; i < getObus().size(); ++i) {
+
+            Obus o = getObus().get(i);
+            o.move(deltaTime);
+            for (int j = 0; j < grid.length; ++j) {
+                GameElement[] ges = grid[j];
+                for (int k = 0; k < ges.length; ++k) {
+                    GameElement ge = ges[k];
+
+                    if (ge instanceof MurBrique && ge.getHitbox().overlaps(o.getHitbox())) {
+                        remplacerGrid(j, k, new ElementVide());
+                        obus.remove(o);
+                    } else if (ge instanceof MurFer && ge.getHitbox().overlaps(o.getHitbox())) {
+                        obus.remove(o);
+                    }
+                }
+            }
+            for (int j = 0; j < npcs.size(); ++j) {
+                if (npcs.get(j).getHitbox().overlaps(o.getHitbox())) {
+                    npcs.remove(npcs.get(j));
+                    obus.remove(o);
+                }
+            }
+
+        }
+    }
 
     /**
      * Instantiate a World, load all the needed files.
@@ -45,41 +106,57 @@ public class World {
             }
         }
 
-        text = Gdx.files.local("config/player_start.json").readString();
-        float[] playerPosition = {};
-        playerPosition = json.fromJson(playerPosition.getClass(), text);
-        joueur.setX(playerPosition[0]);
-        joueur.setY(playerPosition[1]);
+        text = Gdx.files.local("config/config.json").readString();
+        Config config = new Config();
 
-        text = Gdx.files.local("config/player_speed.json").readString();
-        float playerSpeed = 0;
-        playerSpeed = json.fromJson(float.class, text);
-        joueur.setVitesse(playerSpeed);
+        config = json.fromJson(Config.class, text);
+        joueur.setX(config.playerPos[0]);
+        joueur.setY(config.playerPos[1]);
 
-        text = Gdx.files.local("config/player-width-height.json").readString();
-        float[] playerWidthHeight = {};
-        playerWidthHeight = json.fromJson(playerWidthHeight.getClass(), text);
-        joueur.setWidth(playerWidthHeight[0]);
-        joueur.setHeight(playerWidthHeight[1]);
+        joueur.setVitesse(config.playerSpeed);
+
+        joueur.setWidth(config.playerWidth);
+        joueur.setHeight(config.playerHeight);
 
 
-        text = Gdx.files.local("config/obus-max.json").readString();
-        bulletMax = json.fromJson(int.class, text);
+        bulletMax = config.obusMax;
+        bulletSpeed = config.obusSpeed;
 
-        text = Gdx.files.local("config/obus-speed.json").readString();
-        bulletSpeed = json.fromJson(float.class, text);
-
-
-        text = Gdx.files.local("config/npc_start.json").readString();
-
-        float[][] npcPos = {};
-        npcPos = json.fromJson(npcPos.getClass(), text);
-        for (float[] npcPo : npcPos) {
-            TankNpc npc = new TankNpc();
+        for (float[] npcPo : config.npcStart) {
+            TankNpc npc;
+            if (npcPo[3] == 0f) {
+                npc = new TankChasseur1();
+            } else if (npcPo[3] == 1f) {
+                npc = new TankChasseur2();
+            } else {
+                throw new RuntimeException("NPC type uninplemented");
+            }
             npc.setX(npcPo[0]);
             npc.setY(npcPo[1]);
+            npc.setWidth(1);
+            npc.setHeight(1);
+
+            if (npcPo[2] == 0f) {
+                npc.setDirection(Direction.HAUT);
+            } else if (npcPo[2] == 1f) {
+                npc.setDirection(Direction.GAUCHE);
+            } else if (npcPo[2] == 2f) {
+                npc.setDirection(Direction.BAS);
+            } else if (npcPo[2] == 3f) {
+                npc.setDirection(Direction.DROITE);
+            } else {
+                throw new RuntimeException("Not a direction");
+            }
+
             npcs.add(npc);
         }
+
+        intervaleAvion = config.plane;
+
+    }
+
+    public ArrayList<TankNpc> getNpcs() {
+        return npcs;
     }
 
     public void spawnNewBullet(float x, float y, Direction direction) {
@@ -99,7 +176,7 @@ public class World {
     }
 
     public void deleteUnreachableObus() {
-        for(int i = 0; i < obus.size(); ++i) {
+        for (int i = 0; i < obus.size(); ++i) {
             if (obus.get(i).getX() < -.5 || obus.get(i).getY() < -.5 || obus.get(i).getX() > 20 || obus.get(i).getY() > 20)
                 obus.remove(obus.get(i));
         }
@@ -111,6 +188,27 @@ public class World {
 
     public TankJoueur getJoueur() {
         return joueur;
+    }
+
+    void checkCollisionAndMove(float newX, float newY, ElementDynamique ed, Direction dirIfNotMoved) {
+        Rectangle hb = new Rectangle(newX, newY, ed.getWidth(), ed.getHeight());
+
+        boolean canMove = true;
+        for (GameElement[] ges : getGrid()) {
+            for (GameElement ge : ges) {
+                if (ge instanceof ElementDur && ge.getHitbox().overlaps(hb)) {
+                    canMove = false;
+                }
+            }
+        }
+
+        ed.setCanMove(canMove);
+        if (canMove) {
+            ed.setX(newX);
+            ed.setY(newY);
+        } else {
+            ed.setDirection(dirIfNotMoved);
+        }
     }
 
     public void updateJoueur(float modX, float modY, Direction direction, boolean moved) {
@@ -125,23 +223,108 @@ public class World {
         float newX = getJoueur().getX() + modX;
         float newY = getJoueur().getY() + modY;
 
-        Rectangle newPlayerHitBox = new Rectangle(newX, newY, joueur.getWidth(), joueur.getHeight());
+        checkCollisionAndMove(newX, newY, joueur, direction);
 
-        Rectangle wallPos;
+    }
 
-        boolean canMove = true;
-        for(GameElement[] ges : getGrid()) {
-            for (GameElement ge: ges) {
-                if (ge instanceof ElementDur && ge.getHitbox().overlaps(newPlayerHitBox)) {
-                    canMove = false;
-                    wallPos = ge.getHitbox();
-                }
+    private static final int NPC_SPEED = 2;
+    private static final double EPSILON = 0.1;
+
+    boolean goToWall(ElementDynamique ed) {
+        if (ed.getHitbox().getX() % 1 < 0.1) {
+            if (Direction.GAUCHE == ed.getDirection()) {
+                return grid[Math.round(ed.getX()) - 1][Math.round(ed.getY())] instanceof ElementDur;
+            }
+            if (Direction.DROITE == ed.getDirection()) {
+                return grid[Math.round(ed.getX()) + 1][Math.round(ed.getY())] instanceof ElementDur;
             }
         }
+        return false;
+    }
 
-        if (canMove) {
-            getJoueur().setX(newX);
-            getJoueur().setY(newY);
+    public void checkNpc(float deltaTime) {
+
+        elapsedPlaneTime += deltaTime;
+        if (elapsedPlaneTime > intervaleAvion) {
+            elapsedPlaneTime = elapsedPlaneTime - intervaleAvion;
+             Avion avion = new Avion();
+             avion.genDir();
+             avions.add(avion);
+        }
+
+        for (Avion a: avions) {
+            a.move();
+        }
+
+        for (TankNpc npc : npcs) {
+
+            Direction dir = null;
+            float modX = 0, modY = 0;
+            if (npc instanceof TankChasseur1) {
+                float comparisionX = joueur.getX() - npc.getX();
+                float comparisionY = joueur.getY() - npc.getY();
+
+                if (Math.abs(comparisionX) > EPSILON && npc.canMove()) {
+                    if (comparisionX < 0) {
+                        modX -= deltaTime;
+                        dir = Direction.GAUCHE;
+                    } else {
+                        modX += deltaTime;
+                        dir = Direction.DROITE;
+                    }
+                } else if (Math.abs(comparisionY) > EPSILON) {
+                    if (comparisionY < 0) {
+                        modY -= deltaTime;
+                        dir = Direction.BAS;
+                    } else {
+                        modY += deltaTime;
+                        dir = Direction.HAUT;
+                    }
+                }
+            }
+            else {
+                dir = npc.getDirection();
+                if (!npc.canMove()) {
+                    dir = Direction.values()[(int) (Math.random()*10) % 4];
+                    System.out.println("World.checkNpc dir" + dir + "!canmove");
+                }
+                switch (npc.getDirection()) {
+                    case BAS:
+                        modY -= deltaTime;
+                        break;
+                    case HAUT:
+                        modY += deltaTime;
+                        break;
+                    case DROITE:
+                        modX += deltaTime;
+                        break;
+                    case GAUCHE:
+                        modX -= deltaTime;
+                        break;
+                }
+            }
+            updateNpc(npc, modX, modY, dir);
         }
     }
+
+    public void updateNpc(TankNpc npc, float modX, float modY, Direction direction) {
+
+        Direction oldNpcDir = npc.getDirection();
+
+        if (direction != null) {
+           npc.setDirection(direction);
+        }
+
+        modX *= NPC_SPEED;
+        modY *= NPC_SPEED;
+
+        float newX = npc.getX() + modX;
+        float newY = npc.getY() + modY;
+
+        if (npc instanceof TankChasseur1)
+            checkCollisionAndMove(newX, newY, npc, oldNpcDir);
+        else
+            checkCollisionAndMove(newX, newY, npc, npc.getDirection());
+    }
+
 }
